080ad4d171fceffb2bfc3da8881ff8293f2e8ae7
Markus Quaritsch <quam@qwws.net>
2021-05-24 11:49:38 +0200

    extending regexes to allow parsing units, extend parsing to parse by DSM ID

--- example.js
+++ example.js
@@ -2,3 +2,11 @@ const parser   = require('./index');
 const telegram = require('fs').readFileSync('example-telegram.txt', 'utf-8');
 
 console.log(parser.parse(telegram).objects);
+
+console.log("-------------");
+
+console.log(parser.parse(telegram, parser.ParseTypes.NumericIDs).objects);
+
+console.log("-------------");
+
+console.log(parser.parse(telegram, parser.ParseTypes.TextBased | parser.ParseTypes.NumericIDs).objects);
